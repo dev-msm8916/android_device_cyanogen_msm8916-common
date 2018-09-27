a5fda1b4995ef6b924745540f7c25ba0e9846e6e
Quallenauge <Hamsi2k@freenet.de>
2018-09-27 19:44:14 +0200

    msm8916-common: Add dummy perf profile.
    Since lineageos 15.1 the power hal doesn't use the
    perf_lock_use_profile() method anymore.
    It was used to apply performance profiles.
    On startup the perfd daemon is looking for at least
    one profile which is used as default.
    So there is only a perf-profiles0.conf to make the
    daemon happy.
    
    Removes log spam:
    E ANDR-PERF-MPCTL: Invalid profile no. 0, total profiles 0 only
    
    Change-Id: I170cda016f99ab2ab00fbefe78ddde9757235c44

--- msm8916.mk
+++ msm8916.mk
@@ -208,6 +208,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+
 # Radio
 PRODUCT_PACKAGES += \
     libcnefeatureconfig \
